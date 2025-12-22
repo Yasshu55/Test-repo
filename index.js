19a3db64f9dc18d488545b8afc9fa1d5925bbbf4
Yashwanth sai <95294297+Yasshu55@users.noreply.github.com>
2025-12-22 15:08:05 +0530

    Error change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello there')
+    res.send('Hello there'
 });
 
 app.listen(3000, () => console.log("App listening on - 3000"))
