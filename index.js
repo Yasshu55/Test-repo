e2b95b784ff50f2997b07af8beb5e617706b2152
Yashwanth sai <95294297+Yasshu55@users.noreply.github.com>
2025-12-26 14:27:16 +0530

    Sum of numbers

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import {sum} from 'sum2number'
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello there'
+    res.send("Sum : " + sum(2,3));
 });
 
 app.listen(3000, () => console.log("App listening on - 3000"))
